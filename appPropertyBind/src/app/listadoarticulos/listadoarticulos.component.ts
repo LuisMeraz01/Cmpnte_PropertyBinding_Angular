caa3e4a8d3e3ee7085eede9cecd684f6f89915cf
LuisMeraz01227 <luis_mbs05@hotmail.com>
2023-11-02 22:32:30 -0700

    Funciones y estructura del componente listado

--- appPropertyBind/src/app/listadoarticulos/listadoarticulos.component.ts
+++ appPropertyBind/src/app/listadoarticulos/listadoarticulos.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-listadoarticulos',
   templateUrl: './listadoarticulos.component.html',
   styleUrls: ['./listadoarticulos.component.css']
 })
-export class ListadoarticulosComponent {
+export class ListadoarticulosComponent implements OnInit {
 
+  @Input() datos:any;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
